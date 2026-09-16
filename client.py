9f659daef63d60ce61f58d1b8061f43f8e597511
nadav <nadavdant123@gmail.com>
2026-09-16 11:02:14 +0300

    client and server

---
+++ client.py
@@ -0,0 +1,54 @@
+import argparse
+import sys
+import socket
+import struct
+
+###########################################################
+####################### YOUR CODE #########################
+###########################################################
+
+
+# send data to server with little endian
+def send_data(server_ip, server_port, data):
+    """
+    Send data to server in address (server_ip, server_port).
+    """
+
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
+        client_socket.connect((server_ip, server_port))
+        client_socket.send(struct.pack("I", len(data)))
+        client_socket.sendall(data.encode("utf-8"))
+
+
+###########################################################
+##################### END OF YOUR CODE ####################
+###########################################################
+
+
+def get_args():
+    parser = argparse.ArgumentParser(description="Send data to server.")
+    parser.add_argument("server_ip", type=str, help="the server's ip")
+    parser.add_argument("server_port", type=int, help="the server's port")
+    parser.add_argument("data", type=str, help="the data")
+    print(parser.parse_args())
+    return parser.parse_args()
+
+
+def main():
+    """
+    Implementation of CLI and sending data to server.
+    """
+    args = get_args()
+    send_data(args.server_ip, args.server_port, args.data)
+    """
+    try:
+        send_data(args.server_ip, args.server_port, args.data)
+        print("Done.")
+    except Exception as error:
+        print(f"ERROR: {error}")
+        return 1
+    # """
+
+
+if __name__ == "__main__":
+    sys.exit(main())
---
+++ server.py
@@ -0,0 +1,44 @@
+import socket
+import argparse
+import sys
+import struct
+
+
+# print the massages from the server
+def run_server(ip, port):
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
+        server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server_socket.bind((ip, port))
+        server_socket.listen()
+
+        while True:
+            connection, address = server_socket.accept()
+            msg = connection.recv(4)
+            num = struct.unpack("I", msg)[0] + 2**16 * struct.unpack("I", msg)[1]
+            message = connection.recv(num).decode("utf-8")
+            print("Received data: ", message)
+            connection.close()
+
+
+def get_args():
+    parser = argparse.ArgumentParser(description="Listen to server...")
+    parser.add_argument("server_ip", type=str, help="the server's ip")
+    parser.add_argument("server_port", type=int, help="the server's port")
+    return parser.parse_args()
+
+
+def main():
+    """
+    Implementation of CLI and sending data to server.
+    """
+    args = get_args()
+    try:
+        run_server(args.server_ip, args.server_port)
+        print("Done.")
+    except Exception as error:
+        print(f"ERROR: {error}")
+        return 1
+
+
+if __name__ == "__main__":
+    sys.exit(main())
